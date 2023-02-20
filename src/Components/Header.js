12bb9877d6a7af548178a0a33de252dc91b325a2
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-02-20 18:44:30 +0200

    Header text is now a link to homepage. Also added some minor css for the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,16 +2,17 @@ import React from 'react'
 import './Header.css'
 import { UilMoon } from '@iconscout/react-unicons'
 import { useGlobalContext } from '../context'
+import { Link } from 'react-router-dom'
 
 function Header() {
   const {theme, changeTheme} = useGlobalContext()
 
   return (
     <header data-theme={theme}>
-        <h1 className='header__header-title'>Where in the world?</h1>
-        <div className='header__theme-switcher'>
+        <Link to="/countries_api" className='header-title'>Where in the world?</Link>
+        <div className='theme-switcher'>
             <UilMoon />
-            <span className='header__theme-switcher-text' onClick={changeTheme}>{theme === 'dark' ? 'Light' : 'Dark'} Mode</span>    
+            <span className='theme-switcher-text' onClick={changeTheme}>{theme === 'dark' ? 'Light' : 'Dark'} Mode</span>    
         </div>
     </header>
   )
