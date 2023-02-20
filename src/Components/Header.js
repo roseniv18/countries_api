5d7bb11679113730ee52ac0f1081bd910ca227e5
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-02-20 18:51:35 +0200

    Theme icon is now dynamic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import './Header.css'
-import { UilMoon } from '@iconscout/react-unicons'
+import { UilMoon, UilSun } from '@iconscout/react-unicons'
 import { useGlobalContext } from '../context'
 import { Link } from 'react-router-dom'
 
 function Header() {
   const {theme, changeTheme} = useGlobalContext()
 
+  const icon = theme === "light" ? <UilSun /> : <UilMoon />
+
   return (
     <header data-theme={theme}>
         <Link to="/countries_api" className='header-title'>Where in the world?</Link>
         <div className='theme-switcher'>
-            <UilMoon />
+            {icon}
             <span className='theme-switcher-text' onClick={changeTheme}>{theme === 'dark' ? 'Light' : 'Dark'} Mode</span>    
         </div>
     </header>
