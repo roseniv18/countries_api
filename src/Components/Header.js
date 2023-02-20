c0f3f2e93c4fc1b9c267203282d9ec67d13100ea
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-02-20 18:55:28 +0200

    Bugfix. Clicking on icon now also changes theme.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,15 +7,15 @@ import { Link } from 'react-router-dom'
 function Header() {
   const {theme, changeTheme} = useGlobalContext()
 
-  const icon = theme === "light" ? <UilSun /> : <UilMoon />
+  const icon = theme === "light" ? <UilSun onClick={changeTheme}/> : <UilMoon onClick={changeTheme}/>
 
   return (
     <header data-theme={theme}>
-        <Link to="/countries_api" className='header-title'>Where in the world?</Link>
-        <div className='theme-switcher'>
-            {icon}
-            <span className='theme-switcher-text' onClick={changeTheme}>{theme === 'dark' ? 'Light' : 'Dark'} Mode</span>    
-        </div>
+      <Link to="/countries_api" className='header-title'>Where in the world?</Link>
+      <div className='theme-switcher'>
+          {icon}
+          <span className='theme-switcher-text' onClick={changeTheme}>{theme === 'dark' ? 'Light' : 'Dark'} Mode</span>    
+      </div>
     </header>
   )
 }
